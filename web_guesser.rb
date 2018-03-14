dd4cfb100d2c46a50694c7eeea527a655f95fe3a
Roman Alenskiy <kuperman88@gmail.com>
2018-03-14 19:39:08 +0300

    add attempts counter

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,9 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :number, rand(100)
-get '/' do
-    message = check_guess(params['guess'])
-    erb :index, :locals => {:number => settings.number, :message => message}
+disable :is_guessed
+@@attempts = 6
+get '/' do       
+    
+    if @@attempts == 0
+        @@attempts = 5
+        message = "All attempts are exhausted! New number was succesfully generated!"
+        settings.number = rand(100)
+    else
+        message = check_guess(params['guess'])
+        @@attempts -= 1
+    end
+    if settings.is_guessed
+        settings.number = rand(100)
+        @@attempts = 5
+        settings.is_guessed = false
+    end
+    erb :index, :locals => {:number => settings.number, :message => message, :attempts => @@attempts}
 end
 
 def check_guess(guess)
@@ -15,5 +30,8 @@ def check_guess(guess)
     return "You are close! (too low)" if (guess - settings.number).between?(-5,-1)
     return "Too high!" if guess > settings.number
     return "Too low!" if guess < settings.number
-    return "You god damn right! The secret number is #{guess}" if guess == settings.number
+    if guess == settings.number
+        settings.is_guessed = true
+        return "You god damn right! The secret number is #{guess}. New number was sucessfully generated!" 
+    end
 end
